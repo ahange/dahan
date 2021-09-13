87c338e25e9b26877629c4406446a3738d112049
ahange <853275862@qq.com>
2021-09-14 02:57:27 +0800

    Initialize bugfix/action-button-position-change-recompute

---
+++ frontend/src/Editor/Components/RichTextEditor.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+import { DraftEditor } from './DraftEditor';
+import { resolveReferences } from '@/_helpers/utils';
+
+export const RichTextEditor = function RichTextEditor({
+  id,
+  width,
+  height,
+  component,
+  onComponentClick,
+  currentState,
+  onComponentOptionChanged
+}) {
+
+  const placeholder = component.definition.properties.placeholder.value;
+  const widgetVisibility = component.definition.styles?.visibility?.value || true;
+
+
+  let parsedWidgetVisibility = widgetVisibility;
+  
+  try {
+    parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
+  } catch (err) { console.log(err); }
+
+  function handleChange(html) {
+    onComponentOptionChanged(component, 'value', html);
+  }
+
+  return (
+    <div style={{ width: `${width}px`, height: `${height}px`, display:parsedWidgetVisibility ? '' : 'none'  }} onClick={event => {event.stopPropagation(); onComponentClick(id, component)}}>
+      <DraftEditor
+        handleChange={handleChange}
+        height={height}
+        width={width}
+        placeholder={placeholder}
+      ></DraftEditor>
+    </div>
+  );
+};
