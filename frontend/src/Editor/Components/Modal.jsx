7cb0b5cbfc9a8d78a821ae3e95897e2c4e4f6d00
ahange <853275862@qq.com>
2021-09-14 02:57:32 +0800

    Initialize overflow-datepicker

---
+++ frontend/src/Editor/Components/Modal.jsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { default as BootstrapModal } from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import { SubCustomDragLayer } from '../SubCustomDragLayer';
+import { SubContainer } from '../SubContainer';
+import { ConfigHandle } from '../ConfigHandle';
+
+export const Modal = function Modal({
+  id,
+  component,
+  height,
+  mode,
+  containerProps
+}) {
+  const [show, showModal] = useState(false);
+  const parentRef = useRef(null);
+
+  const titleProp = component.definition.properties.title;
+  const title = titleProp ? titleProp.value : '';
+
+  const sizeProp = component.definition.properties.size;
+  const size = sizeProp ? sizeProp.value : 'lg';
+
+  useEffect(() => {
+    const componentState = containerProps.currentState.components[component.name];
+    const canShowModel = componentState ? componentState.show : false;
+    showModal(canShowModel);
+  }, [containerProps.currentState.components[component.name]]);
+
+  function hideModal() {
+    containerProps.onComponentOptionChanged(component, 'show', false);
+    showModal(false);
+  }
+
+  return (
+    <div>
+      <BootstrapModal
+        contentClassName="modal-component"
+        show={show}
+        container={document.getElementsByClassName('canvas-area')[0]}
+        size={size}
+        backdrop={true}
+        keyboard={true}
+        enforceFocus={false}
+        animation={false}
+        onEscapeKeyDown={() => showModal(false)}
+      >
+        {containerProps.mode === 'edit' && 
+          <ConfigHandle 
+            id={id} 
+            component={component}
+            configHandleClicked={containerProps.onComponentClick}
+          /> 
+        }
+        <BootstrapModal.Header>
+          <BootstrapModal.Title>
+            {title}
+          </BootstrapModal.Title>
+          <div>
+            <Button variant="light" size="sm" onClick={hideModal}>
+              x
+            </Button>
+          </div>
+        </BootstrapModal.Header>
+
+        <BootstrapModal.Body style={{ height }} ref={parentRef}>
+            <SubContainer
+              parent={id}
+              {...containerProps}
+              parentRef={parentRef}
+            />
+            <SubCustomDragLayer
+              snapToGrid={true}
+              parentRef={parentRef}
+              parent={id}
+              currentLayout={containerProps.currentLayout}
+            />
+        </BootstrapModal.Body>
+      </BootstrapModal>
+    </div>
+  );
+};
---
+++ frontend/src/Editor/Components/QrScanner/QrScanner.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import QrReader from 'react-qr-reader';
+import ErrorModal from './ErrorModal';
+import { resolveReferences } from '@/_helpers/utils';
+
+export const QrScanner = function QrScanner({
+  component, onEvent, onComponentOptionChanged, currentState
+}) {
+
+  const handleError = async (errorMessage) => {
+    console.log(errorMessage);
+    setErrorOccured(true);
+  };
+
+  const handleScan = async (data) => {
+    if (data != null) {
+      onEvent('onDetect', { component, data: data });
+      onComponentOptionChanged(component, 'lastDetectedValue', data);
+    };
+  };
+
+  let [errorOccured, setErrorOccured] = useState(false);
+
+  const widgetVisibility = component.definition.styles?.visibility?.value || true;
+
+  let parsedWidgetVisibility = widgetVisibility;
+  
+  try {
+    parsedWidgetVisibility = resolveReferences(parsedWidgetVisibility, currentState, []);
+  } catch (err) { console.log(err); }
+
+  return (
+    <div style={{display:parsedWidgetVisibility ? '' : 'none'}}>
+      {
+        errorOccured ?
+          <ErrorModal />
+        :
+          <QrReader
+            onError={handleError}
+            onScan={handleScan}
+          />
+      }
+    </div>
+  );
+};
