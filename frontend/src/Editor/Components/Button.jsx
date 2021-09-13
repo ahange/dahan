ffda186e12bf0cc81c4db05f4c515f839acd27b2
ahange <853275862@qq.com>
2021-09-14 02:57:22 +0800

    Initialize hotfix/goto-app-listing-issue-rebased-to-main

---
+++ frontend/src/Editor/Components/Button.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { resolveReferences } from '@/_helpers/utils';
+var tinycolor = require("tinycolor2");
+
+export const Button = function Button({
+  id, width, height, component, onComponentClick, currentState
+}) {
+  console.log('currentState', currentState);
+
+  const [loadingState, setLoadingState] = useState(false);
+
+  useEffect(() => {
+    const loadingStateProperty = component.definition.properties.loadingState;
+    if (loadingStateProperty && currentState) {
+      const newState = resolveReferences(loadingStateProperty.value, currentState, false);
+      setLoadingState(newState);
+    }
+  }, [currentState]);
+
+  const text = component.definition.properties.text.value;
+  const backgroundColor = component.definition.styles.backgroundColor.value;
+  const color = component.definition.styles.textColor.value;
+
+  const computedStyles = {
+    backgroundColor,
+    color,
+    width,
+    height,
+    '--tblr-btn-color-darker': tinycolor(backgroundColor).darken(8).toString() 
+  };
+
+  return (
+    <button
+      className={`jet-button btn btn-primary p-1 ${loadingState === true ? ' btn-loading' : ''}`}
+      style={computedStyles}
+      onClick={() => onComponentClick(id, component)}
+    >
+      {text}
+    </button>
+  );
+};
---
+++ frontend/src/Editor/Components/TextInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const TextInput = function TextInput({
+  id,
+  width,
+  height,
+  component,
+  onComponentClick,
+  currentState,
+  onComponentOptionChanged
+}) {
+  console.log('currentState', currentState);
+
+  const placeholder = component.definition.properties.placeholder.value;
+
+  return (
+    <input
+      onClick={() => onComponentClick(id, component)}
+      onChange={(e) => onComponentOptionChanged(component, 'value', e.target.value)}
+      type="text"
+      className="form-control"
+      placeholder={placeholder}
+      style={{ width, height }}
+    />
+  );
+};
