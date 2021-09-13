ab7e8307b06ac4e307c9ae01702e7a274b297013
ahange <853275862@qq.com>
2021-09-14 02:57:06 +0800

    Initialize feature/typesense-integration

---
+++ frontend/src/Editor/DataSourceManager/SourceComponents/index.js
@@ -0,0 +1,33 @@
+import { Elasticsearch } from './Elasticsearch';
+import { Redis } from './Redis';
+import { Postgresql } from './Postgresql';
+import { Mysql } from './Mysql';
+import { Stripe } from './Stripe';
+import { Firestore } from './Firestore';
+import { Restapi } from './Restapi';
+import { Googlesheets } from './Googlesheets';
+import { Slack } from './Slack';
+import { Mongodb } from './Mongodb';
+import { Dynamodb } from './Dynamodb';
+import { Airtable } from './Airtable';
+import { Graphql } from './Graphql';
+import { Mssql } from './Mssql';
+import { Typesense } from './Typesense';
+
+export const SourceComponents = {
+    Elasticsearch,
+    Redis,
+    Postgresql,
+    Mysql,
+    Stripe,
+    Firestore,
+    Restapi,
+    Googlesheets,
+    Slack,
+    Mongodb,
+    Dynamodb,
+    Airtable,
+    Graphql,
+    Mssql,
+    Typesense
+};
